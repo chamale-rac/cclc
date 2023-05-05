fd9f7334a49b5432e84e5e4f503083cacfff7b28
chamale-rac <samuel.chamale23@gmail.com>
2023-05-04 22:48:12 -0600

    Final changes and error page customization

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -7,10 +7,51 @@ export default function ErrorPage() {
   console.error(error)
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+    <div
+      id="error-page"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100vw',
+        height: '90vh',
+        color: '#f94149',
+        fontFamily: "'TT Bricks Trial', sans-serif",
+      }}
+    >
+      <img
+        src="/calc.svg"
+        style={{
+          width: '10rem',
+          height: '10rem',
+        }}
+      />
+      <h1
+        style={{
+          fontSize: '5rem',
+          textAlign: 'center',
+        }}
+      >
+        Oops!
+      </h1>
+      <p
+        style={{
+          fontSize: '2.4rem',
+          fontWeight: '400',
+          textAlign: 'center',
+        }}
+      >
+        Sorry, an unexpected error has occurred.
+      </p>
+      <p
+        style={{
+          fontSize: '1.6rem',
+          fontWeight: '500',
+
+          textAlign: 'center',
+        }}
+      >
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
